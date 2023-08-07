2788f34457e70c25c47b636c2b4e91b6d603a80e
unknown <charaka.20200241@iit.ac.lk>
2023-08-07 17:05:42 +0530

    connected to the database

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const chalk = require('chalk');
-
+const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
+const config = require('./config/config')[env];
 
 require('dotenv').config();
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Connect to the databse
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: process.env.PG_HOST,
+  dialect: config.dialect,
+  pool: {
+    max: parseInt(process.env.PG_MAXCONN),
+    min: 0,
+    acquire: 60000,
+    idle: 10000,
+  },
+});
+sequelize
+  .authenticate()
+  .then(async () => {
+    console.log(chalk.white.bgCyan.bold(`Connection has been established successfully.`));
+  })
+  .catch((error) => {
+    console.log(
+      chalk.white.bgRedBright.bold(` Unable to connect to the database: ${error.message} `)
+    );
+    process.exit();
+  });
+
+// start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(
