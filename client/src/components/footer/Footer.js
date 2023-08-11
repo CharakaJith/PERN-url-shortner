48f4f5b1726f2c1cf10fcc33e969cacaa621b2ea
Charaka Jith Gunasinghe <94886516+CharakaJith@users.noreply.github.com>
2023-08-11 23:09:13 +0530

    Development (#2)
    * minor bug fixes
    
    * favicon added
    
    * react components implemented properly

---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="fixed-footer">
+      <p>&copy; 2023 Jith. All rights reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ client/src/pages/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import './HomePage.css';
+
+import InputUrlBox from '../../components/url-input/InputUrlBox';
+import Footer from '../../components/footer/Footer';
+
+function HomePage() {
+  return (
+    <div>
+      <InputUrlBox />
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
